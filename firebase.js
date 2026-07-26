c55f062329888c140d7fc8b5198f4ded189265df
Lee-png-maker <llee77907@gmail.com>
2026-07-26 15:11:32 +0200

    Add Firebase configuration to firebase.js
    Added Firebase configuration object with necessary keys.

--- firebase.js
+++ firebase.js
@@ -1,1 +1,11 @@
 
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA-Ik-sIZhg3ShrywwJif0It5PH6R17Mu0",
+  authDomain: "lee-mobile-services.firebaseapp.com",
+  projectId: "lee-mobile-services",
+  storageBucket: "lee-mobile-services.firebasestorage.app",
+  messagingSenderId: "507796082180",
+  appId: "1:507796082180:web:8df5734768f261f21b2f51",
+  measurementId: "G-S0Q2TGJJLP"
+};
